83bd0e105e8abcf58dcfe1ed7324f570f8cbb3ac
Tiago rosa da costa <tiagorosadacost@gmail.com>
2023-07-15 08:47:42 -0300

    Resolve error

--- pages/api/gists.ts
+++ pages/api/gists.ts
@@ -18,24 +18,23 @@ export default async (
     res.status(401).json({ message: "User unauthorizated" })
   }
 
-  // const response = await axios.post("https://api.github.com/gists", {
-  //   "public": false,
-  //   "files": {
-  //     [`${randomUUID()}.yaml`]: {
-  //       "content": req.body.content
-  //     }
-  //   }
-  // }, {
-  //   headers: {
-  //     "Accept": "application/vnd.github+json",
-  //     // @ts-ignore
-  //     "Authorization": `Bearer ${session.user.accessToken}`,
-  //     "X-GitHub-Api-Version": "2022-11-28"
-  //   }
-  // }).catch(console.log)
+  const response = await axios.post("https://api.github.com/gists", {
+    "public": false,
+    "files": {
+      [`${randomUUID()}.yaml`]: {
+        "content": req.body.content
+      }
+    }
+  }, {
+    headers: {
+      "Accept": "application/vnd.github+json",
+      // @ts-ignore
+      "Authorization": `Bearer ${session.user.accessToken}`,
+      "X-GitHub-Api-Version": "2022-11-28"
+    }
+  })
 
-  // console.log(session)
-  // // @ts-ignore
-  // console.log(response.data)
+  // @ts-ignore
+  console.log(response.data)
   res.end()
 }
